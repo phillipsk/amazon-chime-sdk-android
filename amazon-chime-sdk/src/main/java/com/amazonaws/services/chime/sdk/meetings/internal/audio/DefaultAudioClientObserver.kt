4222fe3ceacea0d595bb1211993ee17dd0f2e7eb
Nick Hwang <hokyungh@amazon.com>
2020-10-16 00:40:56 -0700

    Fix blocking thread

--- amazon-chime-sdk/src/main/java/com/amazonaws/services/chime/sdk/meetings/internal/audio/DefaultAudioClientObserver.kt
+++ amazon-chime-sdk/src/main/java/com/amazonaws/services/chime/sdk/meetings/internal/audio/DefaultAudioClientObserver.kt
@@ -22,9 +22,8 @@ import com.amazonaws.services.chime.sdk.meetings.session.MeetingSessionStatusCod
 import com.amazonaws.services.chime.sdk.meetings.utils.logger.Logger
 import com.xodee.client.audio.audioclient.AttendeeUpdate
 import com.xodee.client.audio.audioclient.AudioClient
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 
 class DefaultAudioClientObserver(
     private val logger: Logger,
@@ -360,17 +359,17 @@ class DefaultAudioClientObserver(
         }
     }
 
-    private fun handleOnAudioSessionFailed(statusCode: MeetingSessionStatusCode?) = runBlocking {
-        if (audioClient != null) {
-            launch(Dispatchers.Default) {
+    private fun handleOnAudioSessionFailed(statusCode: MeetingSessionStatusCode?) {
+        GlobalScope.launch {
+            if (audioClient != null) {
                 audioClient?.stopSession()
                 DefaultAudioClientController.audioClientState = AudioClientState.STOPPED
                 notifyAudioClientObserver { observer ->
                     observer.onAudioSessionStopped(MeetingSessionStatus(statusCode))
                 }
+            } else {
+                logger.error(TAG, "Failed to stop audio session since audioClient is null")
             }
-        } else {
-            logger.error(TAG, "Failed to stop audio session since audioClient is null")
         }
     }
 }
